6eb6424998cd4f58d9ae0026dd26012b61571252
jaru84 <40066804+jaru84@users.noreply.github.com>
2018-10-19 11:44:53 -0400

    Updated Controller.java
    I need to check if the calling methods will work correctly once the code is mergued on Dev branch.

--- ProjectGroupA/src/main/java/com/fundation/search/controller/Controller.java
+++ ProjectGroupA/src/main/java/com/fundation/search/controller/Controller.java
@@ -26,17 +26,18 @@ public class Controller {
       *	searcher is a object of type Searcher class, Searcher class must be implemented in model package.
       *	val is a object of type Validator class.
       *	file is object of type SearcherFile class where the data related to one file will be stored and managed as object by model, controller and view packages.
-      *	interface is object of type SearcherGUI class where UI should be implemented.*/
-	//private Searcher searcher;
+      *	windowUI is object of type SearchWindow class where the UI is implemented.
+      * searcher is object of type Search class where the process of searching is performed.*/
+	private Search searcher;
 	private Validator val;
 	private SearcherCriteria file;
-	//private SearcherGUI interface;
+	private SearchWindow windowUI;
 	
 	/** 
      *  constructor for Controller class where the objects declared above will be  initialized by its respective constructors.*/
 	public Controller(){
-		//interface= new SearcherGUI();
-		//searcher= new Searcher();
+		windowUI= new SearchWindow();
+		searcher= new Search();
 		val= new Validator();
 		file = new SearcherCriteria();
 	}
@@ -51,60 +52,21 @@ public class Controller {
      *  then will be sent to model to start with search process and then showed under UI. */
 	private void search() {
 		try {
-			file.path= getPath();  
-			file.fileName= getFileName();
-			file.ext= getExtension();
-			file.size=getSize();
-			file.operator=getOperator();
+			file.path= windowUI.getPath();  //not sure if this will work I need to check once mergued on Dev
+			file.fileName= windowUI.getFileName(); //not sure if this will work I need to check once mergued on Dev
+			file.ext= windowUI.getExtension(); //not sure if this will work I need to check once mergued on Dev
+			file.size=windowUI.getSize(); //not sure if this will work I need to check once mergued on Dev
+			file.operator=windowUI.getOperator(); 
 			val.validate(file);			
 			//searcher.search(file);
 			displayResults();
-			}catch (Exception ex) {	System.out.println(ex.getMessage()); }
-	}
-	/**
-     *  method that currently is simulating the process to get value for path. It will change to one call to File object where 
-     *  SearcherGUI class should set the value.*/
-	private String getPath() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-      	System.out.println("Insert a valid path:");
-      	return reader.readLine(); 		
-	}
-	/**
-     *  method that currently is simulating the process to get value for file name. It will change to one call to File object where 
-     *  SearcherGUI class should set the value. */
-	private String getFileName() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-      	System.out.println("Insert a valid file name:");
-		return reader.readLine(); 		
-	}
-	/**
-     *  method that currently is simulating the process to get value for extension. It will change to one call to File object where 
-     *  SearcherGUI class should set the value. */
-	private String getExtension() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-      	System.out.println("Insert a valid extension:");
-		return reader.readLine(); 	
-	}
-	/**
-     *  method that currently is simulating the process to get value for size. It will change to one call to File object where 
-     *  SearcherGUI class should set the value. */
-	private String getSize() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-      	System.out.println("Insert a size value:");
-      	return reader.readLine(); 	
-	}
-	/**
-    *  method that currently is simulating the process to get value for operator. It will change to one call to File object where 
-    *  SearcherGUI class should set the value. */
-	private String getOperator() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-      	System.out.println("Insert an operator:");
-      	return reader.readLine(); 	
+		} catch (Exception ex) {	
+			System.out.println(ex.getMessage()); 
+		}
 	}
 	/**
      *  method used to call to the method in charge to get results for the search process, the method should be in model package.*/
 	private void displayResults() {
-		System.out.println(file.toString());
-		//searcher.resultList;
+		//here will be a for statement calling to model method in charge to save results.
 	}
 }
